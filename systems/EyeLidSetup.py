6b9e396b2b1fd622c8a9a825cc98a6f9138c642a
btwtill <Till.Pfarr@web.de>
2023-09-08 17:36:57 +0200

    wrapped EyelidBuild into function

--- systems/EyeLidSetup.py
+++ systems/EyeLidSetup.py
@@ -6,7 +6,7 @@ import maya.cmds as cmds
 ## FUNCTIONS
 
 #Joint Creation Function for seperate Eyelids
-def createEylidJoints(vtxSelection, *args, **kwargs):
+def createEylidJoints(vtxSelection, _centerPos, _baseName, *args, **kwargs):
 
     #Store Vtx Joints
     vertexJoints = []
@@ -19,15 +19,15 @@ def createEylidJoints(vtxSelection, *args, **kwargs):
         
         #craete joints at vertex position
         cmds.select(cl=1)
-        vtxJnt = cmds.rename(cmds.joint(), baseName + args[0] + "_skn" + str(i))
+        vtxJnt = cmds.rename(cmds.joint(), _baseName + args[0] + "_skn" + str(i))
         vtxPos = cmds.xform(vtxSelection[i], q=True, ws=True, t=True)
         cmds.xform(vtxJnt, ws=True, t=vtxPos)
         vertexJoints.append(vtxJnt)
         
         #create joint at center position 
         cmds.select(cl=1)
-        centerJnt = cmds.rename(cmds.joint(), baseName + args[0] + "_jnt")
-        cmds.xform(centerJnt, ws=True, t=eyeCenterPos)
+        centerJnt = cmds.rename(cmds.joint(), _baseName + args[0] + "_jnt")
+        cmds.xform(centerJnt, ws=True, t=_centerPos)
         parentJoints.append(centerJnt)
         
         #parent vertex joint to center joint
@@ -39,7 +39,7 @@ def createEylidJoints(vtxSelection, *args, **kwargs):
     return parentJoints, vertexJoints
 
 #Create Locators at given Positions and Aim an Object at that Locator
-def createAimedAtLocators(_constraintObjects, _targetPositions, _upVectorObject, *args, **kwargs):
+def createAimedAtLocators(_constraintObjects, _targetPositions, _upVectorObject, _baseName, *args, **kwargs):
     
     #store the created Locators
     outputLocators = []
@@ -48,7 +48,7 @@ def createAimedAtLocators(_constraintObjects, _targetPositions, _upVectorObject,
     for i in range(len(_targetPositions)):
             
         #create and Rename SpaceLocator
-        targetLocator = cmds.rename(cmds.spaceLocator()[0], baseName + args[0] + "_loc")
+        targetLocator = cmds.rename(cmds.spaceLocator()[0], _baseName + args[0] + "_loc")
         
         #get target Position
         targetPos = cmds.xform(_targetPositions[i], q=True, ws=True, t=True)
@@ -66,29 +66,30 @@ def createAimedAtLocators(_constraintObjects, _targetPositions, _upVectorObject,
     return outputLocators   
 
 ## Build
-#base Name
-baseName = "l_eyelid"
+def buildEyeLidSetup():
+    #base Name
+    baseName = "l_eyelid"
 
-#get Selected Vertecies
-vtxSelectionUpperEyelid = cmds.ls(selection=True, flatten=True)
+    #get Selected Vertecies
+    vtxSelectionUpperEyelid = cmds.ls(selection=True, flatten=True)
 
-vtxSelectionLowerEyelid = cmds.ls(selection=True, flatten=True)
+    vtxSelectionLowerEyelid = cmds.ls(selection=True, flatten=True)
 
-#get Eye center Position
-eyeCenterPos = cmds.xform("l_eyeCenterPos", q=True, ws=True, t=True)
+    #get Eye center Position
+    eyeCenterPos = cmds.xform("l_eyeCenterPos", q=True, ws=True, t=True)
 
-#Create an UpVector for aimConstraints
-upVectorObject = cmds.rename(cmds.spaceLocator()[0], baseName + "_UpVector")
-cmds.xform(upVectorObject, ws=True, t=[eyeCenterPos[0], eyeCenterPos[1] + 0.5, eyeCenterPos[2]])
+    #Create an UpVector for aimConstraints
+    upVectorObject = cmds.rename(cmds.spaceLocator()[0], baseName + "_UpVector")
+    cmds.xform(upVectorObject, ws=True, t=[eyeCenterPos[0], eyeCenterPos[1] + 0.5, eyeCenterPos[2]])
 
 
-#create and store the Joints for the upper Eyelid
-upperEyelidParentJoints, upperEyelidSlideJoints = createEylidJoints(vtxSelectionUpperEyelid, "Upper")
-print(upperEyelidParentJoints, upperEyelidSlideJoints)
+    #create and store the Joints for the upper Eyelid
+    upperEyelidParentJoints, upperEyelidSlideJoints = createEylidJoints(vtxSelectionUpperEyelid, eyeCenterPos, baseName, "Upper")
+    print(upperEyelidParentJoints, upperEyelidSlideJoints)
 
-#clean Outliner
-upperEylidGrp = cmds.group(upperEyelidParentJoints, name=baseName + "UpperJoints_grp", world=True)
+    #clean Outliner
+    upperEylidGrp = cmds.group(upperEyelidParentJoints, name=baseName + "UpperJoints_grp", world=True)
 
-#create the Aimed At Locators 
-aimLocators = createAimedAtLocators(upperEyelidParentJoints, upperEyelidSlideJoints, upVectorObject, "Upper")
-print(aimLocators)
+    #create the Aimed At Locators 
+    aimLocators = createAimedAtLocators(upperEyelidParentJoints, upperEyelidSlideJoints, upVectorObject, baseName, "Upper")
+    print(aimLocators)
