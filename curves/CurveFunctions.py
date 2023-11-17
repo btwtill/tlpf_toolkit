f5bb370c28ea6d50238873ce4f9ab854ab15c5b1
btwtill <57750780+btwtill@users.noreply.github.com>
2023-11-17 18:11:04 +0100

    added Curve Function Menu with Snap to Curve Function and Create Curve from Selection

---
+++ curves/CurveFunctions.py
@@ -0,0 +1,74 @@
+from maya import cmds
+
+
+
+#=======================================
+## Snap Selection to Closest Curve Point
+#=======================================
+
+def snapSelectionToClosestCurvePoint():
+    selection = cmds.ls(selection=True)
+
+    selCurve = selection.pop()
+
+    npcNodes = list()
+    cacheTransforms = list()
+    for i in selection:
+        cacheTransform = cmds.createNode("transform", name = f"{i}_cacheTransform")
+        outPutTransform = cmds.createNode("transform", name = f"{i}_outoutTransform")
+        
+        currentPos = cmds.xform(i, query = True, ws = True, m = True)
+        cmds.xform(cacheTransform, m = currentPos, ws = True)
+        
+        npciNode = cmds.createNode("nearestPointOnCurve", name = f"{selCurve}_npciTmp")
+        cmds.connectAttr(f"{selCurve}.worldSpace[0]", f"{npciNode}.inputCurve")
+        for n in "XYZ":
+            cmds.connectAttr(f"{cacheTransform}.translate{n}", f"{npciNode}.inPosition{n}")
+            cmds.connectAttr(f"{npciNode}.position{n}", f"{outPutTransform}.translate{n}")
+        
+        outputPosition = cmds.xform(outPutTransform, query = True, ws = True, m = True)
+        cmds.xform(i, m = outputPosition, ws=True)
+        
+        npcNodes.append(npciNode)
+        cacheTransforms.append(cacheTransform)
+        cacheTransforms.append(outPutTransform)
+        
+    cmds.delete(npcNodes)
+    cmds.delete(cacheTransforms)
+
+#=======================================
+## Snap Selection to Closest Curve Point - END
+#=======================================
+
+#=======================================
+## Create Linear Curve from Selection
+#=======================================
+
+def createLinearCurveFromSelectionConfig():
+    selection = cmds.ls(flatten = True, os = True)
+    createLinearCurveFromSelection(selection)
+
+def createLinearCurveFromSelection(_objSelection, _crvName = "curve"):
+
+    #define Curve Point Positions and Knots
+    pointPositions = []
+    knots = []
+
+    #iterate over selection
+    for i in range(len(_objSelection)):
+
+        #get woldspace point position and store it in the point position list
+        newPos = cmds.xform(_objSelection[i], q=True, ws=True, t=True)
+        pointPositions.append(newPos)
+
+        #add Knot into Knot list
+        knots.append(i)
+    
+    newCurve = cmds.curve(d=1, p=pointPositions, k=knots, n = _crvName)
+    cmds.rename(cmds.listRelatives(newCurve, shapes=True), _crvName + "Shape")
+
+    return newCurve, knots
+
+#=======================================
+## Create Linear Curve from Selection -END
+#=======================================
