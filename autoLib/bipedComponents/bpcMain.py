8bed4ab9709cf344a641814c3f716d1ab2673945
btwtill <57750780+btwtill@users.noreply.github.com>
2024-05-25 22:37:03 +0200

    defining a first main componet version

--- autoLib/bipedComponents/bpcMain.py
+++ autoLib/bipedComponents/bpcMain.py
@@ -1,23 +1,39 @@
 import maya.cmds as cmds
-import generalFunctions as gf
+from tlpf_toolkit.autoLib import autolibGlobalVariables as globalVar
+from tlpf_toolkit.autoLib import generalFunctions as gf
 
 #compose character Main component
-def composeMainComponent(parent, numOfMainControls = 1, 
+def composeMainComponent(parent, numOfMainControlOffsets = 1, 
                          mainCtrlShapeName = "circle", offsetCtrlShapeName = "circle", 
                          size = 40, name = "main"):
-    #define side Prefix
-    side = "M"
-    nodeUsageTypeTransform = "srt"
-    nodeUsageTypeControl = "ctrl"
 
     #create outliner structure
     mainComponentTopNode, mainComponentStructureNodes = gf.createRigComponent(name, parent)
 
     #create god node transform
-    godNodeTransform = cmds.createNode("transform", name = f"{side}_{name}_godNode_{nodeUsageTypeTransform}")
+    godNodeTransform = cmds.createNode("transform", name = f"{globalVar.CENTERDECLARATION}_{name}_godNode_{globalVar.NODEUSAGETYPETRANSFORM}")
     cmds.parent(godNodeTransform, mainComponentStructureNodes[2])
     gf.untouchableTransform([godNodeTransform])
 
 
     #create main Controls
-    mainCtrlNode = cmds.circle(name = f"{side}_{name}_global_{nodeUsageTypeControl}")
+    mainCtrlNode = cmds.circle(name = f"{globalVar.CENTERDECLARATION}_{name}_global_{globalVar.NODEUSAGETYPECONTROL}", radius = size, nr = [0,1,0])
+    cmds.parent(mainCtrlNode, godNodeTransform)
+
+    #create main Offset Controls
+    offsetControls = []
+    for offCtrl in range(numOfMainControlOffsets):
+
+        #parent first offset ctrl to main ctrl
+        if len(offsetControls) == 0:
+            newOffsetCtrl = cmds.circle(name = f"{globalVar.CENTERDECLARATION}_{name}_globalOffset{offCtrl}_{globalVar.NODEUSAGETYPECONTROL}", 
+                                        radius = size - ((offCtrl + 1)*4), nr = [0,1,0])
+            cmds.parent(newOffsetCtrl, mainCtrlNode)
+            offsetControls.append(newOffsetCtrl)
+        #parent all next offset ctrls to the one before themselves
+        else:
+            newOffsetCtrl = cmds.circle(name = f"{globalVar.CENTERDECLARATION}_{name}_globalOffset{offCtrl}_{globalVar.NODEUSAGETYPECONTROL}", 
+                                        radius = size - (offCtrl*2), nr = [0,1,0])
+            cmds.parent(newOffsetCtrl, offsetControls[offCtrl - 1])
+            offsetControls.append(newOffsetCtrl)
+            
